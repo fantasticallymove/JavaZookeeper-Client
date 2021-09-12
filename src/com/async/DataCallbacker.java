b45d307e990e9733c137e4d8aa0546137cefd960
謝竣富 <fufen758@gmail.com>
2021-09-12 15:42:36 +0800

    20210912

---
+++ src/com/async/DataCallbacker.java
@@ -0,0 +1,39 @@
+package com.async;
+
+import org.apache.zookeeper.AsyncCallback.DataCallback;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.Master;
+
+
+public class DataCallbacker implements DataCallback
+{
+	private Logger LOG = LoggerFactory.getLogger(this.getClass());
+	private Master master;
+	
+	public DataCallbacker(Master master)
+	{
+		this.master = master;
+	}
+	
+	@Override
+	public void processResult(int resultCode, String path, Object ctx, byte[] data, Stat stat) {
+		
+		LOG.info("Data: {} , {}",new String(data),stat);		//觀測數據
+		
+		switch(Code.get(resultCode))
+		{			
+			case CONNECTIONLOSS :					//發生網路分區 連線中斷 就在嘗試檢查一次 recursive
+						master.checkMaster();
+						break;
+			case NONODE :							//當確認無資料表示無節點，執行創建
+						master.runMaster();
+						break;
+			default:
+				LOG.info("Code :{} , Node exists already!",Code.get(resultCode));
+		}		
+	}
+}
---
+++ src/com/async/StringCallbacker.java
@@ -0,0 +1,38 @@
+package com.async;
+
+import org.apache.zookeeper.AsyncCallback.StringCallback;
+import org.apache.zookeeper.KeeperException.Code;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.Master;
+
+public class StringCallbacker implements StringCallback
+{
+	private Logger LOG = LoggerFactory.getLogger(this.getClass());
+	private Master master;
+	
+	public StringCallbacker(Master master)
+	{
+		this.master =  master;
+	}
+	
+	@Override
+	public void processResult(int resultCode, String path, Object ctx, String nodeName) {
+		switch(Code.get(resultCode))
+		{
+			case CONNECTIONLOSS :			//發生網路異常.分區時處理方式
+					master.checkMaster();	//zkCli再次執行檢查
+					Master.isLeader = false;
+					break;
+			case OK: 						//成功建立時處理方式
+					master.checkMaster();
+					Master.isLeader = true;
+					break;
+			default:						//未建立成功時處理方式
+					Master.isLeader = false;
+		}
+		LOG.info("Code: {} , Content: {} , NodeName: {}",Code.get(resultCode),new String((byte[])ctx),nodeName);
+		System.out.println("I am "+(Master.isLeader ? "" : "not ")+"leader!");
+	}
+}
